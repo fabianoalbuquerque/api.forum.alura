3fcd42e522acf1bdf1214f6ad57c098d5b52728b
fabianoalbuquerque <fabiano.santos@sensedia.com>
2022-03-10 12:13:06 -0300

    general: inicializando o projeto

---
+++ src/main/java/com/apialuraforum/ApiAluraForumApplication.java
@@ -0,0 +1,13 @@
+package com.apialuraforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAluraForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAluraForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apialuraforum/ApiAluraForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apialuraforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAluraForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
